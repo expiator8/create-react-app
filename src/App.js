3649709cc76c87f0ad0c5111fbf8e14f18f57158
이주형's Mac <leejuhyung@ijuhyeongsui-MacBookPro.local>
2022-02-08 13:56:17 +0900

    EFFECTS_useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState, userEffet, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev)  => prev + 1);
+  console.log("i run all the time")
+
+  useEffect(()=>{
+    console.log("CALL THE API....");
+  }, []);
   return (
     <div>
-      <h1>Wellcom back!</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
